a8c07291a289ad523762996a5e59850847402952
chimerablack1 <christopherparke@zoho.com>
2020-10-31 18:45:39 -0400

    fixed the issue with the title

--- plot1.py
+++ plot1.py
@@ -5,16 +5,10 @@ class DrawChart:
     """
     Draw a bar chart using the plotly library
     """
-    def __init__(self, regions, dataSet):
-        self.regions = regions
-        self.dataSet = dataSet
-
-
-    def barChart(self, arg):
-        regions = self
-        southY = arg[0]
-        northY = arg[1]
-        im = [Image.open("greenApple.png"), Image.open("orange.png")]
+    def barChart(regions, dataset):
+        southY = args[0]
+        northY = args[1]
+        img = Image.open("salesBy.png")
 
         fig = go.Figure(data=[
             #Draw green bars for Apples
@@ -36,24 +30,13 @@ class DrawChart:
                     texttemplate="%{y:$,}")
         ])
 
-        #add apple image
-        fig.add_layout_image(
-            dict(
-                source=im[0],
-                xref="x domain", yref="paper",
-                x=0.5, y=1.06,
-                sizex=.05, sizey=.05,
-                xanchor="right", yanchor="bottom"
-            )
-        )
-
         #add orange image
         fig.add_layout_image(
             dict(
-                source=im[1],
+                source=img,
                 xref="paper", yref="paper",
-                x=0.6, y=1.06,
-                sizex=.05, sizey=.05,
+                x=0.85, y=1,
+                sizex=0.5, sizey=0.5,
                 xanchor="right", yanchor="bottom"
             )
         )
@@ -65,13 +48,11 @@ class DrawChart:
                             tickprefix = '$', 
                             separatethousands=True
                         ), 
-                            title="Sales by Region:      Apples vs      Oranges",
-                            title_x=0.5,
                             paper_bgcolor="rgb(245,245,219)", 
                             plot_bgcolor="rgb(245,245,219)")
         fig.show()
 
 regions = ['South', 'North']
 dataset = [[1234.56, 3241.45],[4329.92, 2987.43]]
-barChart = DrawChart.barChart(regions,dataset)
+barChart = DrawChart.barChart(regions, dataset)
 barChart
